b118b45a1184cbeff5fc39ac7cb71a1319b48484
Philip Greenwald <philip@avidest.com>
2016-09-16 11:25:40 -0400

    location form saves location and spits out lat and long

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,5 +4,22 @@ class LocationsController < ApplicationController
 	end
 
 	def create
+		@location = Location.new(location_params)
+		if @location.save
+			redirect_to location_path(@location)
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@location = Location.find(params[:id])
+		render text: "Coordinates are #{@location.convert_to_coordinates}"
+	end
+
+	private
+
+	def location_params
+		params.require(:location).permit(:street_address, :city, :state)
 	end
 end
